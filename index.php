be2a49e4f2b2c690b09c59d53c46379d92554c79
Eduardo <eduardo_110298@hotmail.com>
2018-06-04 19:25:12 -0400

    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="resources/bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="resources/bootstrap/css/bootstrap.min.css">
+	<title>Encuesta ULA - UDO</title>
+</head>
+<body>
+	<div class="container">
+		<p>hola</p>
+		<p>hola</p>
+	</div>
+</body>
+</html>
