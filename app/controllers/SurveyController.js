f3c6b0099d0c9679802aafcb47dc72cdeef9166a
Eduardo <eduardo_110298@hotmail.com>
2018-06-07 21:03:31 -0400

    Auto stash before merge of "master" and "origin/master"

--- app/controllers/SurveyController.js
+++ app/controllers/SurveyController.js
@@ -1,5 +1,9 @@
 app.controller("SurveyController", function($scope, $webService, $window, $compile) {
-	$scope.action = function($event){
+	$scope.traerEncuestas = function(){
 		alert("hola")
 	}
+	$scope.traerEncuestas();
+	// $scope.action = function($event){
+	// 	alert("hola")
+	// }
 });
